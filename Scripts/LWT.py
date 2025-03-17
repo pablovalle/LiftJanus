200f6a9c4d31380e45c647e23b3ebf6bdd2526f2
Pablo Valle <pvalle@mondragon.edu>
2025-03-17 11:07:06 +0100

    Major Revision

--- Scripts/LWT.py
+++ Scripts/LWT.py
@@ -12,25 +12,45 @@ guided_withoutDD=list()
 LWT_unguided=list()
 LWT_guided=list()
 LWT_guided_withoutDD=list()
+mass=list()
+capacity=list()
+mass_capacity=list()
+installation="ExperimentsInstallation2"
 
 for run in runs:
-    file_guided=pd.read_csv(os.path.join("..\Experiments_withDD","ExperimentsScenario2","guided",str(run),"report"+str(run)+".csv"))
+    file_guided=pd.read_csv(os.path.join("../Experiments_withDD",installation,"guided",str(run),"report"+str(run)+".csv"))
     guided.append(file_guided)
     LWT_guided.append(np.asarray(file_guided['LWT_avg']))
 
 
-    file_unguided=pd.read_csv(os.path.join("..\Experiments_withDD","ExperimentsScenario2","unguided",str(run),"report"+str(run)+".csv"))
+    file_unguided=pd.read_csv(os.path.join("../Experiments_withDD",installation,"unguided",str(run),"report"+str(run)+".csv"))
     unguided.append(file_unguided)
     LWT_unguided.append(np.asarray(file_unguided['LWT_avg']))
 
 for run in runs_withoutDD:
-    file_guided_withoutDD=pd.read_csv(os.path.join("..\Experiments_withoutDD","ExperimentsScenario2","guided",str(run),"report"+str(run)+".csv"))
+    file_guided_withoutDD=pd.read_csv(os.path.join("../Experiments_withoutDD",installation,"guided",str(run),"report"+str(run)+".csv"))
     guided_withoutDD.append(file_guided_withoutDD)
     LWT_guided_withoutDD.append(np.asarray(file_guided_withoutDD['LWT_avg']))
 
+for i in range(0, len(runs)):
+    mass_file=pd.read_csv(os.path.join("../Baseline", installation, "Run_"+str(i), "Pareto_iteration_mass.csv"))
+    mass.append(np.asarray(mass_file['LWT']))
+    capacity_file=pd.read_csv(os.path.join("../Baseline", installation, "Run_"+str(i), "Pareto_iteration_capacity.csv"))
+    capacity.append(np.asarray(capacity_file['LWT']))
+    mass_capacity_file=pd.read_csv(os.path.join("../Baseline", installation, "Run_"+str(i), "Pareto_iteration_mass_capacity.csv"))
+    mass_capacity.append(np.asarray(mass_capacity_file['LWT']))
 
 mean_guided=list()
 std_guided=list()
+
+mean_mass=list()
+std_mass=list()
+
+mean_capacity=list()
+std_capacity=list()
+
+mean_mass_capacity=list()
+std_mass_capacity=list()
 
 mean_unguided=list()
 std_unguided=list()
@@ -41,6 +61,9 @@ std_unguided_withoutDD=list()
 
 auc_guided=list()
 auc_unguided=list()
+auc_mass=list()
+auc_capacity=list()
+auc_mass_capacity=list()
 
 
 time_intervals=[i for i in range(1,100)]
@@ -49,45 +72,81 @@ for i in range(0,len(LWT_guided[0])):
     guided_values=list()
     unguided_values=list()
     unguided_values_withoutDD=list()
+    mass_values=list()
+    capacity_values=list()
+    mass_capacity_values=list()
     for j in range(0,len(LWT_guided)):
         guided_values.append(LWT_guided[j][i])
         unguided_values.append(LWT_unguided[j][i])
+        mass_values.append(mass[j][i])
+        capacity_values.append(capacity[j][i])
+        mass_capacity_values.append(mass_capacity[j][i])
     for j in range(0,len(LWT_guided_withoutDD)):
         unguided_values_withoutDD.append(LWT_guided_withoutDD[j][i])
     mean_guided.append(np.mean(guided_values))
     mean_unguided.append(np.mean(unguided_values))
+    mean_mass.append(np.mean(mass_values))
+    mean_capacity.append(np.mean(capacity_values))
+    mean_mass_capacity.append(np.mean(mass_capacity_values))
     mean_unguided_withoutDD.append(np.mean(unguided_values_withoutDD))
     std_guided.append(np.std(guided_values))
     std_unguided.append(np.std(unguided_values))
     std_unguided_withoutDD.append(np.std(unguided_values_withoutDD))
+    std_mass.append(np.std(mass_values))
+    std_capacity.append(np.std(capacity_values))
+    std_mass_capacity.append(np.std(mass_capacity_values))
 for i in range(0,len(LWT_guided)):
-    auc_guided.append(np.trapz(y=LWT_guided[i], x=time_intervals))
-    auc_unguided.append(np.trapz(y=LWT_unguided[i], x=time_intervals))
+    auc_guided.append(np.trapezoid(y=LWT_guided[i], x=time_intervals))
+    auc_unguided.append(np.trapezoid(y=LWT_unguided[i], x=time_intervals))
+    auc_mass.append(np.trapezoid(y=mass[i][:-1], x=time_intervals))
+    auc_capacity.append(np.trapezoid(y=capacity[i][:-1], x=time_intervals))
+    auc_mass_capacity.append(np.trapezoid(y=mass_capacity[i][:-1], x=time_intervals))
 
-print(auc_guided)
-print(auc_unguided)
+print(np.asarray(auc_guided))
+print(np.asarray(auc_unguided))
+print(np.asarray(auc_mass))
+print(np.asarray(auc_capacity))
+print(np.asarray(auc_mass_capacity))
 print(mean_guided[-1])
 print(mean_unguided[-1])
 print(mean_unguided_withoutDD[-1])
 
+#Lipings baseline Installation 1
+#mass=[0, 0, 0, 0, 0, 46.1000, 0, 0]
+#capacity=[123.0200, 104.7000, 48.6714, 114.7250, 78.1250, 68.2625, 116.5500, 65.5500]
+#mass_capacity=[166.3250, 126.6000, 91.9500, 132.4400, 124.6333, 122.1333, 78.6167, 105.5700]
+
+#Lipings baseline Installation 2
+#mass=[19.9750, 11.8000, 38.1250, 11.9750, 11.8000, 13.5500, 11.8000, 14.6000]
+#capacity=[164.2286, 89.8000, 79.9143, 128.5857, 144.4875, 150.4444, 101.5889, 75.7444]
+#mass_capacity=[168.3300, 82.1100, 53.8100, 133.5300, 58.4100, 79.3600, 152.3100, 63.9300]
 
 
+plt.plot(time_intervals, mean_guided, label='LiftJanus', color='#1f77b4')
+plt.fill_between(time_intervals, np.asanyarray(mean_guided)-np.asanyarray(std_guided), np.asanyarray(mean_guided)+np.asanyarray(std_guided), color='#1f77b4', alpha=0.3)
+
+plt.plot(time_intervals, mean_unguided,'--', label='BaseJanus', color='red')
+plt.fill_between(time_intervals, np.asanyarray(mean_unguided)-np.asanyarray(std_unguided), np.asanyarray(mean_unguided)+np.asanyarray(std_unguided), color='red', alpha=0.3)
 
 
-plt.plot(time_intervals, mean_guided, label='Guided')
-plt.fill_between(time_intervals, np.asanyarray(mean_guided)-np.asanyarray(std_guided), np.asanyarray(mean_guided)+np.asanyarray(std_guided), alpha=0.3)
 plt.ylabel("Average LWT difference (s)",weight='bold')
 plt.xlabel("# of iteration",weight='bold')
 plt.title("Average Longest Waiting Time difference between members",weight='bold')
-plt.ylim([0, None])
 plt.xlim([0,100])
+plt.ylim([0,200])
+plt.plot(time_intervals, mean_unguided_withoutDD, '.',label='LiftJanus non-minimized', color='green')
+plt.fill_between(time_intervals, np.asanyarray(mean_unguided_withoutDD)-np.asanyarray(std_unguided_withoutDD), np.asanyarray(mean_unguided_withoutDD)+np.asanyarray(std_unguided_withoutDD), color='green', alpha=0.3)
 
-plt.plot(time_intervals, mean_unguided,'--', label='Unguided')
-plt.fill_between(time_intervals, np.asanyarray(mean_unguided)-np.asanyarray(std_unguided), np.asanyarray(mean_unguided)+np.asanyarray(std_unguided), alpha=0.3)
-plt.legend()
-plt.plot(time_intervals, mean_unguided_withoutDD, '.',label='Guided non-minimized')
-plt.fill_between(time_intervals, np.asanyarray(mean_unguided_withoutDD)-np.asanyarray(std_unguided_withoutDD), np.asanyarray(mean_unguided_withoutDD)+np.asanyarray(std_unguided_withoutDD), alpha=0.3)
-plt.legend()
+plt.plot(time_intervals, mean_mass, 'v',label='UncerRobua C1' , markersize=4, color='#e377c2')
+plt.fill_between(time_intervals, np.asanyarray(mean_mass)-np.asanyarray(std_mass), np.asanyarray(mean_mass)+np.asanyarray(std_mass), color='#e377c2', alpha=0.1)
+
+plt.plot(time_intervals, mean_capacity, 's',label='UncerRobua C2', markersize=3, color='purple')
+plt.fill_between(time_intervals, np.asanyarray(mean_capacity)-np.asanyarray(std_capacity), np.asanyarray(mean_capacity)+np.asanyarray(std_capacity),color='purple', alpha=0.1)
+
+plt.plot(time_intervals, mean_mass_capacity, '*',label='UncerRobua C3', markersize=4,color='orange')
+plt.fill_between(time_intervals, np.asanyarray(mean_mass_capacity)-np.asanyarray(std_mass_capacity), np.asanyarray(mean_mass_capacity)+np.asanyarray(std_mass_capacity),color='orange', alpha=0.1)
+
+plt.legend(fontsize="10",loc ="best")
 plt.savefig("RQ1_Scenario2_new.pdf", format="pdf", bbox_inches="tight")
 plt.show()
 
--- Scripts/experiment_analyzer.m
+++ Scripts/experiment_analyzer.m
@@ -1,11 +1,11 @@
 clc; clear; close all;
-initialPath="..\Experiments_withDD\ExperimentsScenario1";
+initialPath="..\Experiments_withDD\ExperimentsInstallation2";
 addpath(initialPath+"\guided\");
 addpath(initialPath+"\unguided\");
 
 numOfExecs=9;
 numOfIterations=99;
-
+baselineRuns=7;
 
 %% Get the data from the excel files
 
@@ -21,6 +21,17 @@ for i=2:numOfExecs
     archive_guided(i-1,:,:)=table2array(importArchiveResults(path_guided_archive));
     archive_unguided(i-1,:,:)=table2array(importArchiveResults(path_unguided_archive));
 end
+
+
+for i=0:baselineRuns
+    baselinePath="..\Baseline\ExperimentsInstallation2\Run_"+num2str(i)+"\";
+    archive_baseline_mass{i+1}=table2array(importBaseline(baselinePath+"Pareto_mass.csv"));
+    archive_baseline_capacity{i+1}=table2array(importBaseline(baselinePath+"Pareto_capacity.csv"));
+    archive_baseline_mass_capacity{i+1}=table2array(importBaseline(baselinePath+"Pareto_mass_capacity.csv"));
+
+end
+
+
 
 %% Comparision of guided vs unguided for the on-running data
 
@@ -65,6 +76,9 @@ figure; plot(avg_dist_unguided,"r--"); hold on;title("Average Distance between m
 % LWT
 lwt_guided=getLWT(archive_guided);
 lwt_unguided=getLWT(archive_unguided);
+lwt_baseline_mass=getbaselineLWT(archive_baseline_mass);
+lwt_baseline_capacity=getbaselineLWT(archive_baseline_capacity);
+lwt_baseline_mass_capacity=getbaselineLWT(archive_baseline_mass_capacity);
 
 [H, pValue, SWstatistic]=swtest(lwt_guided,0.05);
 if H==1 
@@ -78,21 +92,58 @@ if H==1
 else
     disp("Normally distributed")
 end
-xx=[lwt_guided(:,1),lwt_unguided(:,1) ];
+[H, pValue, SWstatistic]=swtest(lwt_baseline_mass,0.05);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(lwt_baseline_capacity,0.05);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(lwt_baseline_mass_capacity,0.05);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+disp("Approach VS Unguided")
+xx=[lwt_guided(:,1),lwt_unguided(:,1)];
 p_lwt=anova1(xx);
-
 A12_lwt = Atest(lwt_guided(:,1),lwt_unguided(:,1));
 d_lwt=computeCohen_d(lwt_guided(:,1),lwt_unguided(:,1));
 disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
 
-figure
-boxplot([lwt_guided(:,1),lwt_unguided(:,1)],'Notch','on','Labels',{'Guided','Unguided'})
-title('LWT difference guided vs unguided')
+disp("Approach VS Baseline C1")
+xx=[lwt_guided(:,1);lwt_baseline_mass(:,1)];
+p_lwt = anova1(xx);
+A12_lwt = Atest(lwt_guided(:,1),lwt_baseline_mass(:,1));
+d_lwt=computeCohen_d(lwt_guided(:,1),lwt_baseline_mass(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+disp("Approach VS Baseline C2")
+xx=[lwt_guided(:,1),lwt_baseline_capacity(:,1)];
+p_lwt=anova1(xx);
+A12_lwt = Atest(lwt_guided(:,1),lwt_baseline_capacity(:,1));
+d_lwt=computeCohen_d(lwt_guided(:,1),lwt_baseline_capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+disp("Approach VS Baseline C3")
+xx=[lwt_guided(:,1),lwt_baseline_mass_capacity(:,1)];
+p_lwt=anova1(xx);
+A12_lwt = Atest(lwt_guided(:,1),lwt_baseline_mass_capacity(:,1));
+d_lwt=computeCohen_d(lwt_guided(:,1),lwt_baseline_mass_capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
 
 %% Dist to member
-dist_guided=reshape(archive_guided(:,:,5),[],1);
-dist_unguided=reshape(archive_unguided(:,:,5),[],1);
-
+dist_guided=getHD(archive_guided);
+dist_unguided=getHD(archive_unguided);
+dist_baseline_mass=getbaselineHD(archive_baseline_mass);
+dist_baseline_capacity=getbaselineHD(archive_baseline_capacity);
+dist_baseline_mass_capacity=getbaselineHD(archive_baseline_mass_capacity);
 
 [H, pValue, SWstatistic]=swtest(dist_guided);
 if H==1 
@@ -106,33 +157,77 @@ if H==1
 else
     disp("Normally distributed")
 end
-xx=[dist_guided(:,1),dist_unguided(:,1) ];
-p_dist=anova1(xx);
+[H, pValue, SWstatistic]=swtest(dist_baseline_mass);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(dist_baseline_capacity);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(dist_baseline_mass_capacity);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
 
-A12_dist = Atest(dist_guided(:,1),dist_unguided(:,1));
-d_dist=computeCohen_d(dist_guided(:,1),dist_unguided(:,1));
-disp("The guided version dominates with an A12 value of "+num2str(1-A12_dist)+" with a significance value of "+num2str(p_dist)+" and cohen values of: "+num2str(d_dist))
+disp("Approach VS Unguided")
+xx=[dist_guided(:,1),dist_unguided(:,1)];
+p_hd = anova1(xx, [],'off');
+A12_hd = Atest(dist_guided(:,1),dist_unguided(:,1));
+d_hd=computeCohen_d(dist_guided(:,1),dist_unguided(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_hd)+" with a significance value of "+num2str(p_hd)+" and cohen values of: "+num2str(d_hd))
 
-figure
-boxplot([dist_guided(:,1),dist_unguided(:,1)],'Notch','on','Labels',{'Guided','Unguided'})
+disp("Approach VS Baseline C1")
+xx=[dist_guided(:,1),dist_baseline_mass(:,1)];
+p_hd = anova1(xx, [],'off');
+A12_hd = Atest(dist_guided(:,1),dist_baseline_mass(:,1));
+d_hd=computeCohen_d(dist_guided(:,1),dist_baseline_mass(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_hd)+" with a significance value of "+num2str(p_hd)+" and cohen values of: "+num2str(d_hd))
 
-title('Distance to member guided vs unguided')
+disp("Approach VS Baseline C2")
+xx=[dist_guided(:,1),dist_baseline_capacity(:,1)];
+p_hd = anova1(xx, [],'off');
+A12_hd = Atest(dist_guided(:,1),dist_baseline_capacity(:,1));
+d_hd=computeCohen_d(dist_guided(:,1),dist_baseline_capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_hd)+" with a significance value of "+num2str(p_hd)+" and cohen values of: "+num2str(d_hd))
+
+disp("Approach VS Baseline C3")
+xx=[dist_guided(:,1),dist_baseline_mass_capacity(:,1)];
+p_hd = anova1(xx, [],'off');
+A12_hd = Atest(dist_guided(:,1),dist_baseline_mass_capacity(:,1));
+d_hd=computeCohen_d(dist_guided(:,1),dist_baseline_mass_capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_hd)+" with a significance value of "+num2str(p_hd)+" and cohen values of: "+num2str(d_hd))
 
 
 %% ScatterPlot of # of different parameters vs LWT
 [lwt_guided, changes_guided]=getDataArchive(archive_guided);
 [lwt_unguided, changes_unguided]=getDataArchive(archive_unguided);
+[lwt_baseline_mass, changes_baseline_mass]=getDataArchiveBaseline(archive_baseline_mass);
+[lwt_baseline_capacity, changes_baseline_capacity]=getDataArchiveBaseline(archive_baseline_capacity);
+[lwt_baseline_mass_capacity, changes_baseline_mass_capacity]=getDataArchiveBaseline(archive_baseline_mass_capacity);
+
 
 figure;
 hold on
 plot(changes_guided, lwt_guided,'sb','MarkerFaceColor','blue', 'MarkerSize', 7)
 plot(changes_unguided,lwt_unguided, '.r', 'MarkerSize', 20)
+plot(changes_baseline_mass,lwt_baseline_mass, '*', 'MarkerSize', 7)
+plot(changes_baseline_capacity,lwt_baseline_capacity, '+', 'MarkerSize', 7)
+plot(changes_baseline_mass_capacity,lwt_baseline_mass_capacity, '^', 'MarkerSize', 7, 'MarkerFaceColor',[0.4660 0.6740 0.1880])
+
 title("LWT difference and hamming distance between members")
 xlabel('Hamming distance', 'FontWeight','bold') 
 ylabel('LWT difference between pairs (s)', 'FontWeight','bold') 
-legend({'guided','unguided'});
+legend({'LiftJanus','BaseJnaus', 'UncerRobua - C1', 'UncerRobua - C2', 'UncerRobua - C3'}, "FontSize",7); 
+
 %xlim([0,25]);
-xlim([0,0.005]);
+%xlim([0,0.005]);
 ylim([0,350]);
 
 %% # of different parameters
@@ -163,30 +258,91 @@ ylim([0 30]);
 title('# of changes guided vs unguided')
 %%
 % AUC (area under the curve)
-changes_guided=[10477.1375, 10115.474999999999, 9852.285, 9580.210000000001, 10350.642499999998, 9562.9075, 11332.915, 10642.245000000003]';
-changes_unguided=[5539.451249999999, 7879.374374999999, 3996.2875000000013, 5515.171250000001, 5952.593125, 6559.913124999999, 5789.652500000001, 6325.2112499999985]';
+%Installation 1
+guided=[10477.1375, 10115.474999999999, 9852.285, 9580.210000000001, 10350.642499999998, 9562.9075, 11332.915, 10642.245000000003]';
+unguided=[5539.451249999999, 7879.374374999999, 3996.2875000000013, 5515.171250000001, 5952.593125, 6559.913124999999, 5789.652500000001, 6325.2112499999985]';
+mass=[0 0 0 0 0 0 0 0 ]';
+capacity=[7959.42 7863.00125 8807.29875 8650.31625 7936.8125 9437.98375 9170.22875 8592.37875]';
+mass_capacity=[10010.0675 7944.41375 8583.58 9117.70875 8397.07875 8280.65875 7870.01125 8883.9075 ]';
+%Installation 2
+% guided=[10379.2375  9187.78   11437.765  10469.6625  9303.1825 11028.435 10929.0675 11606.6775]';
+% unguided=[6004.894375 7096.6475   6833.973125 5918.1525   3239.0175   6124.79 5870.27125  7163.315625]';
+% mass=[105.08125 0. 329.86 0. 173.075 0. 49.9875 27.4125]';
+% capacity= [11374.325 13316.8575 14015.97 12543.3425 12869.87375 14056.73125 12657.2075 13655.725]';
+% mass_capacity= [13954.6475 13654.1525 13247.2525 13039.255 12727.63 12421.01625 12411.79875 13647.60375]';
 
-[H, pValue, SWstatistic]=swtest(changes_guided);
+[H, pValue, SWstatistic]=swtest(guided);
 if H==1 
     disp("Not normally distributed")
 else
     disp("Normally distributed")
 end
-[H, pValue, SWstatistic]=swtest(changes_unguided);
+[H, pValue, SWstatistic]=swtest(unguided);
 if H==1 
     disp("Not normally distributed")
 else
     disp("Normally distributed")
 end
-xx=[changes_guided(:,1);changes_unguided(:,1) ];
-[p_changes,tbl,stats] = kruskalwallis(xx,[],'off');
+[H, pValue, SWstatistic]=swtest(mass);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(capacity);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
+[H, pValue, SWstatistic]=swtest(mass_capacity);
+if H==1 
+    disp("Not normally distributed")
+else
+    disp("Normally distributed")
+end
 
-A12_changes = Atest(changes_guided(:,1),changes_unguided(:,1));
-d_changes=computeCohen_d(changes_guided(:,1),changes_unguided(:,1));
-disp("The guided version dominates with an A12 value of "+num2str(A12_changes)+" with a significance value of "+num2str(p_changes)+" and cohen values of: "+num2str(d_changes))
-figure
-boxplot([changes_guided(:,1),changes_unguided(:,1)],'Notch','on','Labels',{'Guided','Unguided'})
-title('Area Under the Curve')
+disp("Approach VS Unguided")
+xx=[guided(:,1),unguided(:,1)];
+p_lwt=anova1(xx);
+A12_lwt = Atest(guided(:,1),unguided(:,1));
+d_lwt=computeCohen_d(guided(:,1),unguided(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+disp("Approach VS Baseline C1")
+xx=[guided(:,1),mass(:,1)];
+groupLabels = [ones(size(guided(:,1))); 2*ones(size(mass(:,1)))];
+p_lwt = kruskalwallis(xx, [],'off');
+A12_lwt = Atest(guided(:,1),mass(:,1));
+d_lwt=computeCohen_d(guided(:,1),mass(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+disp("Approach VS Baseline C2")
+xx=[guided(:,1),capacity(:,1)];
+groupLabels = [ones(size(guided(:,1))); 2*ones(size(mass(:,1)))];
+p_lwt = kruskalwallis(xx, [],'off');
+A12_lwt = Atest(guided(:,1),capacity(:,1));
+d_lwt=computeCohen_d(guided(:,1),capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+disp("Approach VS Baseline C3")
+xx=[guided(:,1),mass_capacity(:,1)];
+p_lwt=anova1(xx);
+A12_lwt = Atest(guided(:,1),mass_capacity(:,1));
+d_lwt=computeCohen_d(guided(:,1),mass_capacity(:,1));
+disp("The guided version dominates with an A12 value of "+num2str(A12_lwt)+" with a significance value of "+num2str(p_lwt)+" and cohen values of: "+num2str(d_lwt))
+
+
+
+% xx=[changes_guided(:,1);changes_unguided(:,1) ];
+% [p_changes,tbl,stats] = kruskalwallis(xx,[],'off');
+% 
+% A12_changes = Atest(changes_guided(:,1),changes_unguided(:,1));
+% d_changes=computeCohen_d(changes_guided(:,1),changes_unguided(:,1));
+% disp("The guided version dominates with an A12 value of "+num2str(A12_changes)+" with a significance value of "+num2str(p_changes)+" and cohen values of: "+num2str(d_changes))
+% figure
+% boxplot([changes_guided(:,1),changes_unguided(:,1)],'Notch','on','Labels',{'Guided','Unguided'})
+% title('Area Under the Curve')
 
 %% Functions
 
@@ -204,6 +360,41 @@ function lwt=getLWT(archive)
 
     lwt=reshape(lwt_matrices2,[],1);
 end
+function lwt=getbaselineLWT(archive)
+
+    for i=1:size(archive,2)
+        for j=1:size(archive{i},1)
+            lwt_matrices(i,j)=archive{i}(j,3);
+        end
+        lwt_matrices2(i)=mean(lwt_matrices(i,:));
+    end
+ 
+    lwt=reshape(lwt_matrices2,[],1);
+end
+
+function hamming_dist=getbaselineHD(archive)
+
+    for i=1:size(archive,2)
+        for j=1:size(archive{i},1)
+            hd_matrices(i,j)=archive{i}(j,4);
+        end
+        hd_matrices2(i)=mean(hd_matrices(i,:));
+    end
+ 
+    hamming_dist=reshape(hd_matrices2,[],1);
+end
+
+function hamming_dist=getHD(archive)
+
+    for i=1:size(archive,1)
+        for j=1:size(archive,2)
+            hd_matrices(i,j)=archive(i,j,5);
+        end
+        hd_matrices2(i)=mean(hd_matrices(i,:));
+    end
+ 
+    hamming_dist=reshape(hd_matrices2,[],1);
+end
 
 function [lwt,diff_params]=getDataArchive(archive)
 
@@ -213,6 +404,20 @@ function [lwt,diff_params]=getDataArchive(archive)
             index2=j*2;
             lwt_matrices(i,j)=abs(archive(i,index1,3)-archive(i,index2,3));
             diff_params(i,j)=archive(i,index1,5);
+        end
+    end
+
+    lwt=reshape(lwt_matrices,[],1);
+    diff_params=reshape(diff_params,[],1);
+end
+
+function [lwt,diff_params]=getDataArchiveBaseline(archive)
+    count=1;
+    for i=1:size(archive,2)
+        for j=1:size(archive{i},1)
+            lwt_matrices(count)=archive{i}(j,3);
+            diff_params(count)=archive{i}(j,4);
+            count=count+1;
         end
     end
 
